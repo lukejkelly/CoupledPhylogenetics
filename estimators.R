f119b68792961dd11e66d21659a956f6ffb32951
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-03-05 20:45:26 +0000

    Change indexing

--- estimators.R
+++ estimators.R
@@ -1,26 +1,43 @@
-samples_k_to_m <- function(x, k, m) {
-    # x indexed from 0 so x[1] = x_0, we want x_k, ..., x_m
-    inds <- seq.int(k, m) + 1
-    x_km <- x[inds]
-    return(x_km)
+# functions to index sample output
+# TODO: add initial lag/subsample once TraitLab is updated
+# currently, lag l = subsample interval s so
+#   x[1] = x_0, x[2] = x_s = x_l, x[3] = y_2s, ..., x[n+2] = x_(n+1)s
+#               y[2] = y_0,       y[2] = y_s, ...,  y[n+1] = y_ns,
+# where we have vertically aligned the coupled states
+# this will shortly change to allow l = ts so
+#   x[1] = x_0, ..., x[t+1] = x_ts = x_l, ..., x_[t+n+1] = x_(n+t)s
+#                      y[1] = y_0,        ...,   y_[n+1] = y_ns
+# all else equal, the ground truth chain z has the same indexing as y
+
+ind_x <- function(k, m) {
+    x_inds <- seq.int(k, m) + 1
+    return(x_inds)
 }
+ind_y <- function(k, m) {
+    y_inds <- seq.int(k, m) + 1
+    return(y_inds)
+}
+ind_z <- ind_y
 
 monte_carlo_estimator <- function(x, k, m) {
-    # Assume x includes initial state so x[1] = x_0
-    x_km <- samples_k_to_m(x, k, m)
-    mc <- mean(x_km)
+    # (x_k + ... + x_m) / (m - k + 1)
+    mc <- mean(x[ind_x(k, m)])
     return(mc)
 }
 
 unbiased_estimator <- function(x, y, k, m, t) {
-    # Assume x and y both include initial state so x[1] = x_0, y[1] = y_0 and
-    # proceed at lag/subsample l, so x[i] = x_{(i - 1) * l}, y is likewise
+    # monte carlo term mc(x_k:m) + bias correction bc(x_(k+1):(t-1), y_k:(t-2))
     mc <- monte_carlo_estimator(x, k, m)
-    n_inds <- max((t - 1) - (k + 1) + 1, 0)
-    b_inds <- seq.int(k + 1, t - 1, length.out = n_inds)
-    w <- pmin(1, (b_inds - k) / (m - k + 1))
-    d <- x[(b_inds + 1)] - y[(b_inds + 1) - 1]
-    bc <- w %*% d
+    if (k + 1 > t - 1) {
+        bc <- 0
+    } else {
+        w_inds <- seq.int(k + 1, t - 1)
+        x_inds <- ind_x(k + 1, t - 1)
+        y_inds <- ind_y(k, t - 2)
+        w <- pmin(1, (w_inds - k) / (m - k + 1))
+        d <- x[x_inds] - y[y_inds]
+        bc <- w %*% d
+    }
     ue <- mc + bc
-    return(c(mc = mc, bc = bc, ue = ue))
+    return(list(mc = mc, bc = bc, ue = ue))
 }
