d2237b3a22b4cafb139842e7373d5705426c052b
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-03-16 13:13:43 +0100

    Reflecting new folder names

--- analyses/tree_metrics.sh
+++ analyses/tree_metrics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# run from simulations output folder
 for FILE in *_x.nex; do
     sleep 0.25
-    qsub -d . -v TARGET=${FILE/_x.nex/} ../../src/tree_metrics.pbs
+    qsub -d . -v TARGET=${FILE/_x.nex/} ../../analyses/tree_metrics.pbs
 done
