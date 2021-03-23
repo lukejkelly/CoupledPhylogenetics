0e9b6e71b88f753d5d182b7501b1fd673d05946c
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-03-23 18:15:52 +0000

    20210319 simulations

--- simulate/paramConfig.m
+++ simulate/paramConfig.m
@@ -1,10 +1,32 @@
-% 20210218
+% % 20210218
+% list_L = [8, 10];
+% list_root_time = 1e3;
+% list_lambda = [2.5e-2, 5e-2];
+% list_mu = 5e-04;
+% list_beta = 0;
+% list_run_length = struct('coupled', 5e5, 'marginal', 5e6);
+% list_sample_interval = struct('coupled', 5e2, 'marginal', 5e2);
+% list_lag = [5e2, 5e3, 5e4, 5e5];
+% n_chains = 1e2;
+
+% % 20210311
+% list_L = [8];
+% list_root_time = 1e3;
+% list_lambda = [2.5e-2];
+% list_mu = 5e-04;
+% list_beta = 0;
+% list_run_length = struct('coupled', 2.5e5, 'marginal', 2.5e6);
+% list_sample_interval = struct('coupled', 2.5e2, 'marginal', 2.5e2);
+% list_lag = [2.5e2, 2.5e3, 2.5e4];
+% n_chains = 1e2;
+
+% 20210319
 list_L = [8, 10];
 list_root_time = 1e3;
 list_lambda = [2.5e-2, 5e-2];
 list_mu = 5e-04;
 list_beta = 0;
-list_run_length = struct('coupled', 5e5, 'marginal', 5e6);
-list_sample_interval = struct('coupled', 5e2, 'marginal', 5e2);
-list_lag = [5e2, 5e3, 5e4, 5e5];
+list_run_length = struct('coupled', 5e4, 'marginal', 5e6);
+list_sample_interval = struct('coupled', 5e1, 'marginal', 5e2);
+list_lag = [5e1, 5e2, 5e3];
 n_chains = 1e2;
