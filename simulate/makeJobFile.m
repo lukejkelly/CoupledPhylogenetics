2cf8de779e6342195af8669f838231e2f82e5321
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-03-26 16:11:14 +0000

    Change Torque to Slurm

--- simulate/makeJobFile.m
+++ simulate/makeJobFile.m
@@ -1,26 +1,28 @@
 function makeJobFile(job_type)
 
     [dest_dir_local, dest_dir_traitlab] = fileDest();
-    fid = fopen(fullfile(dest_dir_local, sprintf('job-%s.pbs', job_type)), 'w');
+    fid = fopen(fullfile(dest_dir_local, sprintf('job-%s.sh', job_type)), 'w');
 
     fprintf(fid, '#!/bin/bash\n\n');
 
-    fprintf(fid, '#PBS -M kelly@ceremade.dauphine.fr\n');
-    fprintf(fid, '#PBS -m e\n');
-    fprintf(fid, '#PBS -l nodes=1:ppn=1\n');
-    fprintf(fid, '#PBS -o log/kelly.o$PBS_JOBID\n');
-    fprintf(fid, '#PBS -j oe\n\n');
+    % fprintf(fid, '#SBATCH --mail-user=kelly\n');
+    fprintf(fid, '#SBATCH --mail-type=END\n');
+    fprintf(fid, '#SBATCH -N 1\n');
+    fprintf(fid, '#SBATCH -n 1\n');
 
     switch job_type
     case 'a'
-        fprintf(fid, '#PBS -l walltime=48:00:00\n\n');
+        fprintf(fid, '#SBATCH -o log/slurm-%%A_%%a.out\n');
+        fprintf(fid, '#SBATCH -t 48:00:00\n\n');
     case 'b'
-        fprintf(fid, '#PBS -l walltime=240:00:00\n\n');
+        fprintf(fid, '#SBATCH -o log/slurm-%%j.out\n');
+        fprintf(fid, '#SBATCH -t 240:00:00\n\n');
     end
 
     fprintf(fid, 'sleep $[ ($RANDOM %% 60) + 1 ]s\n\n');
 
-    fprintf(fid, 'PAR_FILE=$(LC_NUMERIC="en_GB.UTF-8" \\\n');
+    % fprintf(fid, 'PAR_FILE=$(LC_NUMERIC="en_GB.UTF-8" \\\n');
+    fprintf(fid, 'PAR_FILE=$( \\\n');
     switch job_type
     case 'a'
         fprintf(fid, '           printf "L%%d_r%%e_l%%e_m%%e_b%%e_l%%e" \\\n');
@@ -31,12 +33,12 @@ function makeJobFile(job_type)
     end
 
     % Cluster job starts from home directory
-    fprintf(fid, 'cd TraitLabSDLT-coupled\n');
+    fprintf(fid, 'cd "$HOME"/TraitLabSDLT-coupled\n');
     fprintf(fid, '/usr/local/bin/matlab -nodesktop -nodisplay \\\n');
     switch job_type
     case 'a'
         fprintf(fid, ...
-                '    -r "batchTraitLab(''%s/${PAR_FILE}.par'', ${PBS_ARRAYID}); exit"\n', ...
+                '    -r "batchTraitLab(''%s/${PAR_FILE}.par'', $SLURM_ARRAY_TASK_ID); exit"\n', ...
                 fullfile(dest_dir_traitlab, 'pars'));
     case 'b'
         fprintf(fid, ...
--- simulate/makeSubmitFile.m
+++ simulate/makeSubmitFile.m
@@ -1,10 +1,10 @@
 function makeSubmitFile(list_L, list_root_time, list_lambda, list_mu, ...
         list_beta, list_lag, n_chains)
 
-    job_name = '-N ${L}-${ROOT_TIME}-${LAMBDA}-${MU}-${BETA}';
-    var_list = '-v L=${L},ROOT_TIME=${ROOT_TIME},LAMBDA=${LAMBDA},MU=${MU},BETA=${BETA}';
+    job_name = '--job-name=${L}-${ROOT_TIME}-${LAMBDA}-${MU}-${BETA}';
+    var_list = '--export=ALL,L=${L},ROOT_TIME=${ROOT_TIME},LAMBDA=${LAMBDA},MU=${MU},BETA=${BETA}';
 
-    pars_a = sprintf('-t 1-%d', n_chains);
+    pars_a = sprintf('--array=1-%d%%10', n_chains);
     pars_b = '';
 
     fid = fopen(fullfile(fileDest(), 'submit.sh'), 'w');
@@ -32,13 +32,13 @@ function makeSubmitFile(list_L, list_root_time, list_lambda, list_mu, ...
     % Coupled chains get lag argument
     fprintf(fid, '%s', repmat(' ', 1, 24));
     fprintf(fid, ...
-            'qsub %s-${LAG} %s %s,LAG=${LAG} job-a.pbs\n', ...
+            'sbatch %s-${LAG} %s %s,LAG=${LAG} job-a.sh\n', ...
             job_name, pars_a, var_list);
     fprintf(fid, '%sdone\n', repmat(' ', 1, 20));
 
     % Ground truth longer chain
     fprintf(fid, '%s', repmat(' ', 1, 20));
-    fprintf(fid, 'qsub %s %s %s job-b.pbs\n', job_name, pars_b, var_list);
+    fprintf(fid, 'sbatch %s %s %s job-b.sh\n', job_name, pars_b, var_list);
 
     fprintf(fid, '%sdone\n', repmat(' ', 1, 16));
     fprintf(fid, '%sdone\n', repmat(' ', 1, 12));
