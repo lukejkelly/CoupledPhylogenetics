8d624256e9a528de91781c067f562cbeb3713167
Luke Kelly <kelly.luke.joseph@gmail.com>
2021-08-31 11:32:15 +0200

    Tidying up simulation script

--- simulate/paramConfig.m
+++ simulate/paramConfig.m
@@ -1,14 +1,44 @@
-% 20210805
-list_L = 10;
+% % 20210815
+% list_L = [8, 12, 16];
+% list_root_time = 1e3;
+% list_lambda = 1e-1;
+% list_mu = 2.5e-04;
+% list_beta = 0;
+% list_run_length = struct('coupled', 0, 'marginal', 0);
+% list_sample_interval = struct('coupled', 1e2, 'marginal', 1e2);
+% list_lag = [1e5, 3e5, 5e5];
+% n_chains = 100;
+% extras = struct('missing', 0, ...
+%                 'clades', 0, ...
+%                 'ncats', 0, ...
+%                 'kappa', 1 / 10);
+
+% % 20210818
+% list_L = [8, 12, 16];
+% list_root_time = 1e3;
+% list_lambda = 1e-1;
+% list_mu = 2.5e-04;
+% list_beta = 0;
+% list_run_length = struct('coupled', 0, 'marginal', 0);
+% list_sample_interval = struct('coupled', 1e2, 'marginal', 1e2);
+% list_lag = [1e5, 3e5, 5e5];
+% n_chains = 100;
+% extras = struct('missing', 1, ...
+%                 'clades', 1, ...
+%                 'ncats', 3, ...
+%                 'kappa', 1 / 20);
+
+% 20210824: same as above but stronger kappa and more stringent cat prior
+list_L = [8, 12, 16];
 list_root_time = 1e3;
 list_lambda = 1e-1;
 list_mu = 2.5e-04;
-list_beta = 0; %, 2.5e-4];
-list_run_length = struct('coupled', 1e6, 'marginal', 0);
+list_beta = 0;
+list_run_length = struct('coupled', 0, 'marginal', 0);
 list_sample_interval = struct('coupled', 1e2, 'marginal', 1e2);
 list_lag = [1e5, 3e5, 5e5];
 n_chains = 100;
-extras = struct('missing', 0, ...
+extras = struct('missing', 1, ...
                 'clades', 1, ...
-                'ncats', 0, ...
+                'ncats', 2, ...
                 'kappa', 1 / 3);
